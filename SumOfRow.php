bd59584bd96e8439cd27eceff3b271b52611b7d6
Kuritsin Egor <kuritsinegor@mail.ru>
2016-09-24 14:10:37 +0300

    Add files via upload

---
+++ SumOfRow.php
@@ -0,0 +1,48 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Вычисление</title>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html"; charset=utf-8">
+</head>
+
+<body>
+
+<?php
+if ((filter_has_var (INPUT_GET, "m")) && (filter_has_var (INPUT_GET, "m")))
+{
+	$m = $_GET ['m'];
+	$n = $_GET ['n'];
+	echo "Матрица:<br>";
+	for($i = 1; $i < $m + 1; $i++){
+		for($j = 1; $j < $n + 1; $j++){
+			$arr = $i * 10 + $j;
+			$matrix[$arr] = rand(1,100);
+			$current = $current + $matrix[$arr];
+			echo " $matrix[$arr] ";
+		}		
+		if ($current > $max){
+			$max = $current;
+		}
+		$current = 0;
+		echo "<br>";
+	}
+	echo "Ответ: $max";
+}
+else
+{
+	print <<< HERE
+	<form method = "get">
+	<fieldset>
+	<p>Введите значение m: </p>
+	<input type = "text" name = "m" maxlength = 1><br>
+	<p><br>Введите значение n: </p>
+	<input type = "text" name = "n" maxlength = 1><br>
+	<br><input type = "submit">
+	</fieldset>
+	</form>
+HERE;
+}
+?>
+
+</body>
+</html>
